94913d56f120a854e3b09778cff63996ed7a1c0b
ALewandowskii <adam.le1221@gmail.com>
2025-01-15 13:01:30 +0100

    3

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learndwdawdwadawdaw
+          Learndwdawdwadawdawvfdvdxvxd
         </a>
       </header>
     </div>
